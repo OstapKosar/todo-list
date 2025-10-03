d706f1c90f86a4c974856b5c76f7085a379e8b65
OstapKosar <ostapkosar497@gmail.com>
2025-10-03 13:20:08 +0300

    chore: fix ESLint version (8.56.0) and comments

--- frontend/prettier.config.cjs
+++ frontend/prettier.config.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-  printWidth: 120, // ширина рядка
-  tabWidth: 2, // відступи у пробілах
-  useTabs: false, // використовувати пробіли замість табів
-  semi: true, // ставити крапку з комою
-  singleQuote: true, // одинарні лапки
-  trailingComma: 'all', // коми в масивах/обʼєктах/імпортах
-  bracketSpacing: true, // пробіли між дужками обʼєктів
-  arrowParens: 'always', // дужки у стрілкових функціях завжди
-  endOfLine: 'lf', // перенос рядка LF
+  printWidth: 120, // line width
+  tabWidth: 2, // indentation in spaces
+  useTabs: false, // use spaces instead of tabs
+  semi: true, // add semicolon
+  singleQuote: true, // single quotes
+  trailingComma: 'all', // commas in arrays/objects/imports
+  bracketSpacing: true, // spaces between object brackets
+  arrowParens: 'always', // always use parentheses in arrow functions
+  endOfLine: 'lf', // LF line ending
 };
