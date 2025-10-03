be66a79e668d18801bca1c74be135c8bd2da2770
OstapKosar <ostapkosar497@gmail.com>
2025-10-03 10:11:50 +0300

    chore: setup ESLint 8 + Prettier with auto-fix scripts

---
+++ frontend/prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120, // ширина рядка
+  tabWidth: 2, // відступи у пробілах
+  useTabs: false, // використовувати пробіли замість табів
+  semi: true, // ставити крапку з комою
+  singleQuote: true, // одинарні лапки
+  trailingComma: 'all', // коми в масивах/обʼєктах/імпортах
+  bracketSpacing: true, // пробіли між дужками обʼєктів
+  arrowParens: 'always', // дужки у стрілкових функціях завжди
+  endOfLine: 'lf', // перенос рядка LF
+};
