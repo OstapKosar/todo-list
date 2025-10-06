a5a008f3c60726f8ab0a9e0846900a5f47c2a54e
Ostap <ostapkosar497@gmail.com>
2025-10-06 17:03:29 +0300

    refactor: move modal into separate component for optimization

---
+++ frontend/src/components/modal/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { useSelector } from 'react-redux';
+
+import { selectModal } from '@/store/slices/modal/selectors';
+
+interface ModalProps {
+  children: React.ReactNode;
+  modalName: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ children, modalName }) => {
+  const { isOpened, modalName: currentModalName } = useSelector(selectModal);
+
+  useEffect(() => {
+    const originalOverflow = document.body.style.overflow;
+
+    if (isOpened) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = originalOverflow;
+    }
+
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
+  }, [isOpened]);
+
+  if (!isOpened || (modalName && modalName !== currentModalName)) return null;
+
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-xs">
+      <div className="relative w-full h-full flex items-center justify-center p-4">{children}</div>
+    </div>,
+    document.querySelector('#modal-root')!,
+  );
+};
+
+export default Modal;
