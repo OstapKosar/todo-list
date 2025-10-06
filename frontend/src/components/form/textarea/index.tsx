88aa9c8202a904aef7c10e118d863af970f2914d
Ostap <ostapkosar497@gmail.com>
2025-10-06 13:40:21 +0300

    feat: add character count

--- frontend/src/components/form/textarea/index.tsx
+++ frontend/src/components/form/textarea/index.tsx
@@ -1,5 +1,6 @@
 import type { TextareaHTMLAttributes } from 'react';
 import { get, useFormContext } from 'react-hook-form';
+import { useState } from 'react';
 
 import { formElementClasses } from '../../../utils/form-element-classes';
 import { cn } from '../../../utils/tailwind';
@@ -14,9 +15,10 @@ interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 const Textarea = ({ label, name, placeholder, maxLength = 200, ...props }: TextareaProps) => {
   const { register, formState } = useFormContext();
   const error = get(formState.errors, name);
+  const [charCount, setCharCount] = useState(0);
 
   return (
-    <div>
+    <div className="relative">
       <label htmlFor={props.id} className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
         {label}
       </label>
@@ -25,12 +27,30 @@ const Textarea = ({ label, name, placeholder, maxLength = 200, ...props }: Texta
         {...register(name)}
         placeholder={placeholder}
         maxLength={maxLength}
+        onChange={(e) => {
+          setCharCount(e.target.value.length);
+        }}
+        style={{
+          paddingBottom: '2.5rem',
+        }}
         className={cn(formElementClasses, 'max-h-60 min-h-20', {
           'outline-red-500 bg-red-50': error,
           'outline-gray-300 bg-gray-100 dark:outline-gray-600 focus:outline-blue-500': !error,
         })}
       />
       {error && <p className={cn('mt-1 text-sm', { 'text-red-700 dark:text-red-400': error })}>{error.message}</p>}
+      <p
+        className={cn(
+          'text-sm bg-white/95 dark:bg-gray-800/95 text-gray-500 dark:text-gray-400 px-2 py-1 rounded-md absolute bottom-2 right-2',
+          {
+            'text-yellow-700 dark:text-yellow-400': charCount >= maxLength * 0.5 && charCount < maxLength * 0.75,
+            'text-orange-700 dark:text-orange-400': charCount >= maxLength * 0.75 && charCount < maxLength,
+            'text-red-700 dark:text-red-400': charCount >= maxLength,
+          },
+        )}
+      >
+        {charCount}/{maxLength}
+      </p>
     </div>
   );
 };
