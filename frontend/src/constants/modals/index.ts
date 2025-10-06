987988d2ac7c2d3b2ed7ac2f348c1b0dfb24f50a
Ostap <ostapkosar497@gmail.com>
2025-10-06 17:05:48 +0300

    feat: add modal slice, modal constants and modal selector

---
+++ frontend/src/constants/modals/index.ts
@@ -0,0 +1,3 @@
+export const modals = {
+  createProject: 'create-project',
+};
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './slices/theme/slice';
 import projectsReducer from './slices/projects/slice';
+import modalReducer from './slices/modal/slice';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     projects: projectsReducer,
+    modal: modalReducer,
   },
 });
 
