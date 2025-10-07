3f6c2daaee47eea23e44c95254ce3bedea8313c7
Ostap <ostapkosar497@gmail.com>
2025-10-07 16:59:33 +0300

    feat: add project details page

---
+++ frontend/src/pages/project/index.tsx
@@ -0,0 +1,45 @@
+import { useDispatch } from 'react-redux';
+
+import ProjectTasksGrid from '@/components/projects/tasks/grid';
+import TasksModal from '@/components/projects/tasks/modal';
+import { modals } from '@/constants/modals';
+import { openModal } from '@/store/slices/modal/slice';
+import { Link } from 'react-router-dom';
+
+const ProjectDetailsPage = () => {
+  const dispatch = useDispatch();
+
+  const handleOpenTasksModal = () => {
+    dispatch(openModal({ name: modals.tasks }));
+  };
+
+  return (
+    <div className="p-10 min-h-screen flex justify-center items-center">
+      <TasksModal />
+      <div className="flex flex-col h-full bg-gray-200 dark:bg-gray-800 p-10 rounded-lg w-full max-w-[85%]">
+        <div className="flex items-center justify-between w-full mb-6">
+          <h1 className="text-2xl font-bold text-blue-600 dark:text-blue-400">{'project-name'}</h1>
+          <div className="flex justify-between items-center gap-4">
+            <Link
+              to={'/dashboard'}
+              className="text-gray-900 dark:text-gray-300 hover:text-gray-400 text-md rounded-lg transition-colors hover:cursor-pointer"
+            >
+              Go Back
+            </Link>
+            <button
+              className="text-blue-900 dark:text-blue-200 hover:text-blue-400 text-lg rounded-lg transition-colors hover:cursor-pointer"
+              onClick={handleOpenTasksModal}
+            >
+              Task List
+            </button>
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <ProjectTasksGrid />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetailsPage;
