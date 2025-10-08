4d5c97ca4c48be01d793da6633ea2c6da891efab
Ostap <ostapkosar497@gmail.com>
2025-10-08 10:52:17 +0300

    feat: add users, tasks, and projects modules with controllers and services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, TasksModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
