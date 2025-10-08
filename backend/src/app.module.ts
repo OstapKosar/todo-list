f582d43c11eb050d62a41fdee6fcaa79eed7d75b
Ostap <ostapkosar497@gmail.com>
2025-10-08 12:24:57 +0300

    feat: initialize auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, TasksModule, ProjectsModule],
+  imports: [PrismaModule, UsersModule, TasksModule, ProjectsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
