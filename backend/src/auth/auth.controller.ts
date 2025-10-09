b18343c08553ca4dc17aa05c21493305bc7e8ea9
Ostap <ostapkosar497@gmail.com>
2025-10-09 11:32:14 +0300

    feat: implement sign-up and login endpoints

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { User } from '@prisma/client';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('Auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: SignupDto): Promise<User> {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto): Promise<{
+    message: string;
+    userInfo: { id: string; name: string; email: string };
+  }> {
+    return this.authService.login(dto);
+  }
+}
