c58a2c541a20c3879b2e105d025489235b3eb57e
Ostap <ostapkosar497@gmail.com>
2025-10-27 16:45:50 +0200

    feat: integrate JWT, Mail, Config, OTP, and Scheduling modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,25 @@ import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import { OTPModule } from './otp/otp.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [PrismaModule, UsersModule, TasksModule, ProjectsModule, AuthModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({ global: true }),
+    PrismaModule,
+    UsersModule,
+    TasksModule,
+    ProjectsModule,
+    AuthModule,
+    MailModule,
+    OTPModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
