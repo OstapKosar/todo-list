cdb7226b5acec8a5dac20eef5cf133f5025646a5
Ostap <ostapkosar497@gmail.com>
2025-10-28 11:54:59 +0200

    fix: dropdown item styles

--- frontend/src/components/user/menu-dropdown/item/index.tsx
+++ frontend/src/components/user/menu-dropdown/item/index.tsx
@@ -7,12 +7,14 @@ type DropdownItemProps = {
 
 const DropdownItem: React.FC<DropdownItemProps> = ({ children, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      className="w-full text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-md transition-colors"
-    >
-      {children}
-    </button>
+    <div>
+      <button
+        className="w-full text-left p-2 m-0.5 rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-700 dark:text-gray-300 transition-colors"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
 
