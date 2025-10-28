3184c7e8ebb9a6fc2b358d212b9252383615f0f6
Ostap <ostapkosar497@gmail.com>
2025-10-28 09:37:42 +0200

    chore: update configuration and app structure to include toast messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Router from '@/router';
 
 const App = () => {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <ToastContainer position="top-right" closeOnClick draggable theme="colored" />
+    </>
+  );
 };
 
 export default App;
