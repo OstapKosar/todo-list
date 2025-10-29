921496cf1d8c7811244b361cd0e4dddb7de57f01
Ostap <ostapkosar497@gmail.com>
2025-10-29 15:17:41 +0200

    feat: integrate global loader into layout component

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '@/components/header';
 import Footer from '@/components/footer';
+import GlobalLoader from '@/components/global-loader';
+import { useGlobalLoading } from '@/hooks/useGlobalLoading';
 
 const Layout = () => {
+  useGlobalLoading();
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900">
+      <GlobalLoader />
       <MainNavigation />
       <main className="flex-1">
         <Outlet />
