b6c945d8f7ea02e6a04a4a537d94343f892d2973
Ostap <ostapkosar497@gmail.com>
2025-10-30 17:00:33 +0200

    feat: add create project task endpoint

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { TasksService } from './tasks.service';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { CreateProjectTaskDto } from './dto/create-project-task.dto';
 
 @Controller('tasks')
 @ApiTags('Tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @UseGuards(JwtGuard('access'))
+  @ApiBearerAuth()
+  @Post('create')
+  createProjectTask(@Body() dto: CreateProjectTaskDto) {
+    return this.tasksService.createProjectTask(dto);
+  }
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,15 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreateProjectTaskDto } from './dto/create-project-task.dto';
 
 @Injectable()
 export class TasksService {
   constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return this.prisma.projectTask.findMany({
+  async findAll() {
+    return await this.prisma.projectTask.findMany({
       include: {
         project: true,
       },
+    });
+  }
+
+  async createProjectTask(dto: CreateProjectTaskDto) {
+    const { projectId } = dto;
+
+    const existingTask = await this.prisma.projectTask.findFirst({
+      where: {
+        projectId,
+        title: dto.title,
+      },
+    });
+
+    if (existingTask) {
+      throw new BadRequestException('Task with this title already exists');
+    }
+
+    return await this.prisma.projectTask.create({
+      data: { ...dto, projectId },
     });
   }
 }
