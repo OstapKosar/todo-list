1382c5932ff0c35f214fbb211a7abe3388ed9783
Ostap <ostapkosar497@gmail.com>
2025-10-30 11:31:12 +0200

    feat: implement basic project management endpoints

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ProjectsService } from './projects.service';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { Context } from 'src/common/decorators/context.decorator';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { IdDto } from 'src/common/dto/id.dto';
 
 @Controller('projects')
 @ApiTags('Projects')
@@ -14,5 +24,26 @@ export class ProjectsController {
   @Get()
   getAll(@Context('uid') uid: string) {
     return this.projectsService.getAll(uid);
+  }
+
+  @UseGuards(JwtGuard('access'))
+  @ApiBearerAuth()
+  @Get(':id')
+  getProject(@Param() id: IdDto) {
+    return this.projectsService.getProject(id);
+  }
+
+  @UseGuards(JwtGuard('access'))
+  @ApiBearerAuth()
+  @Post()
+  createProject(@Body() dto: CreateProjectDto, @Context('uid') uid: string) {
+    return this.projectsService.createProject(dto, uid);
+  }
+
+  @UseGuards(JwtGuard('access'))
+  @ApiBearerAuth()
+  @Delete(':id')
+  deleteProject(@Param() id: IdDto) {
+    return this.projectsService.deleteProject(id);
   }
 }
--- backend/src/projects/projects.service.ts
+++ backend/src/projects/projects.service.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { IdDto } from 'src/common/dto/id.dto';
 
 @Injectable()
 export class ProjectsService {
@@ -14,5 +20,62 @@ export class ProjectsService {
         tasks: true,
       },
     });
+  }
+
+  async getProject(dto: IdDto) {
+    const { id } = dto;
+
+    const project = await this.prisma.project.findUnique({
+      where: { id },
+      include: {
+        tasks: true,
+      },
+    });
+
+    if (!project) {
+      throw new NotFoundException('Project not found');
+    }
+
+    return project;
+  }
+
+  async createProject(dto: CreateProjectDto, userId: string) {
+    const existingProject = await this.prisma.project.findFirst({
+      where: {
+        ownerId: userId,
+        name: dto.name,
+      },
+    });
+
+    if (existingProject) {
+      throw new BadRequestException(
+        'You already have a project with this name',
+      );
+    }
+
+    const project = await this.prisma.project.create({
+      data: {
+        ...dto,
+        ownerId: userId,
+      },
+    });
+
+    return project;
+  }
+
+  async deleteProject(dto: IdDto) {
+    const { id } = dto;
+
+    const project = await this.prisma.project.findUnique({
+      where: { id },
+    });
+
+    if (!project) {
+      throw new NotFoundException('Project not found');
+    }
+
+    await this.prisma.project.delete({ where: { id } });
+
+    return { message: 'Project deleted successfully' };
   }
 }
