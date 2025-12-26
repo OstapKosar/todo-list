aa5b752fb85a95a941d9c7a546f8c32b76e5c99b
Ostap <ostapkosar497@gmail.com>
2025-12-26 13:29:27 +0200

    style: improve project card layout structure

--- frontend/src/components/projects/card/index.tsx
+++ frontend/src/components/projects/card/index.tsx
@@ -19,29 +19,31 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
   };
 
   return (
-    <div className="flex flex-col justify-center p-6 rounded-lg shadow-sm bg-gray-200 dark:bg-gray-800 hover:shadow-md transition-shadow">
+    <div className="flex flex-col justify-between p-6 rounded-lg shadow-sm bg-gray-200 dark:bg-gray-800 hover:shadow-md transition-shadow">
       <div className="p-2">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-100">{project.name}</h2>
       </div>
       <div className="p-2 pb-4">
         <p className="text-gray-700 dark:text-gray-300 text-md">{project.description}</p>
       </div>
-      <div className="w-full flex justify-center">
-        <MatrixPreview />
-      </div>
-      <div className="p-2 pt-4 flex justify-between gap-4">
-        <button
-          onClick={handleDeleteProject}
-          className="text-red-900 dark:text-red-200 hover:text-red-400 rounded-lg transition-colors hover:cursor-pointer"
-        >
-          Delete Project
-        </button>
-        <Link
-          to={`/dashboard/projects/${project.id}`}
-          className="text-blue-900 dark:text-blue-200 hover:text-blue-400 rounded-lg transition-colors"
-        >
-          View Project
-        </Link>
+      <div>
+        <div className="w-full flex justify-center">
+          <MatrixPreview />
+        </div>
+        <div className="p-2 pt-4 flex justify-between gap-4">
+          <button
+            onClick={handleDeleteProject}
+            className="text-red-900 dark:text-red-200 hover:text-red-400 rounded-lg transition-colors hover:cursor-pointer"
+          >
+            Delete Project
+          </button>
+          <Link
+            to={`/dashboard/projects/${project.id}`}
+            className="text-blue-900 dark:text-blue-200 hover:text-blue-400 rounded-lg transition-colors"
+          >
+            View Project
+          </Link>
+        </div>
       </div>
     </div>
   );
